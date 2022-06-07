0314578210ce6043bc029b7a8c38c13fc209226e
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-07 16:02:13 +0900

    f- upload페이지 view, model추가

--- post/models.py
+++ post/models.py
@@ -1,22 +1,23 @@
 from django.db import models
+from user.models import UserModel
 
-# Create your models here.
-# class Recipe(models.Model):
-#     class Meta:
-#         db_table = "recipe"
-#     def __str__(self):
-#         return self.title
-#     title = models.CharField(max_length=100, null=False)
-#     img_url = models.ImageField(upload_to='uploads/')#CharField(max_length=200, default='')
-#     timecost = models.CharField(max_length=30, null=False)
-#     difficulty = models.CharField(max_length=30, null=False) #SET_NULL:필드값이 사라지면 Null로 바꾼다. null=True일때만 사용할 수 있다
-#     ingredient = models.TextField(null=False)
-#     cookstep = models.TextField(null=False)
-#     ##여기까지 form##
-#     author = models.CharField(max_length=100, default='') #User 생기면 ForeignKey로 꼭 바꿔줘야함!!!!!!
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
+#Create your models here.
+class Recipe(models.Model):
+    class Meta:
+        db_table = "recipe"
+    def __str__(self):
+        return self.title
+    title = models.CharField(max_length=100, null=False)
+    img_url = models.ImageField(upload_to='uploads/')#CharField(max_length=200, default='')
+    timecost = models.CharField(max_length=30, null=False)
+    difficulty = models.CharField(max_length=30, null=False) #SET_NULL:필드값이 사라지면 Null로 바꾼다. null=True일때만 사용할 수 있다
+    ingredient = models.TextField(null=False)
+    cookstep = models.TextField(null=False)
+    ##여기까지 form##
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Timecate(models.Model):
     class Meta:
         db_table = "timecost"
--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Timecate, Diffcate
+from .models import Recipe, Timecate, Diffcate
+from .forms import *
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
@@ -7,14 +8,28 @@ def home(request):
 
 def view_list(request):
     if request.method == 'GET':
-        # recipe = Recipe.objects.all()
-        # chucheon = RecipeChucheon.objects.all()
-        # print(type(chucheon))
-        return render(request, 'list.html')#, {'recipe':recipe})
+        recipe = Recipe.objects.all()
+        return render(request, 'list.html', {'recipe':recipe})
+
 
 def upload_recipes(request):
     if request.method == 'GET':
-        # form = RecipeForm()
-        timecate = Timecate.objects.all()
-        diffcate = Diffcate.objects.all()
-        return render(request, 'upload.html', {'timecost': timecate, 'difficulty': diffcate})
+        ur_user = request.user.is_authenticated
+        if ur_user:
+            timecate = Timecate.objects.all()
+            diffcate = Diffcate.objects.all()
+            return render(request, 'upload.html', {'timecost': timecate, 'difficulty': diffcate})
+        else:
+            return redirect('/')
+
+    elif request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('list/')
+        else:
+            return HttpResponse('fail')
+
+
+
