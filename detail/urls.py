3e538aab7a9df993dad3a606c7711addb739e5db
taein Kim <kti0940@gmail.com>
2022-06-08 17:23:40 +0900

    feat - 댓글 보기, 저장, 삭제 완료

--- detail/urls.py
+++ detail/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('detail/<int:id>', views.view_detail, name='view-detail' ),
     path('detail/like/<int:id>', views.like_post, name='like-post'),
+    path('detail/comment/<int:id>', views.comment_post, name='comment-post'),
+    path('detail/comment/delete/<int:id>', views.comment_delete, name='comment-delete'),
 ]
