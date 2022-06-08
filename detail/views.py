f4a9e6f7a7b6025176cbdec578fb0ffc901b23d5
migdracios <migdracios@gmail.com>
2022-06-08 13:45:44 +0900

    Feat - 좋아요 모델을 참조하여 데이터 추가, 삭제하는 좋아요 API

--- detail/views.py
+++ detail/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, redirect
 from webScrapping.models import DefaultRecipe
 from detail.models import LikeModel
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Create your views here.
 def view_detail(request, id):
     target_recipe = DefaultRecipe.objects.get(id=id)
-
-    return render(request, 'detail.html', {'recipe': target_recipe})
+    target_like = LikeModel.objects.filter(like_recipe=id)
+    if target_like:
+        like_status = True
+    else:
+        like_status = False
+    return render(request, 'detail.html', {'recipe': target_recipe, 'like_status': like_status})
 
 @login_required
 def like_post(request, id):
@@ -21,4 +26,4 @@ def like_post(request, id):
         target_like.like_me = me
         target_like.like_recipe = recipe
         target_like.save()
-    return redirect(f'detail/{id}')
+    return redirect(f'/detail/{id}')
