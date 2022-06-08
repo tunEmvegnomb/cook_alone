b5bce9881f80e5f5820c790b3de93a0662524cdc
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-08 18:52:32 +0900

    검색페이지 필터링 백엔드 파트

--- post/views.py
+++ post/views.py
@@ -14,19 +14,39 @@ def home(request):
         return redirect('/signin')
 
 def view_list(request):
+    total_recipe = DefaultRecipe.objects.count()
     if request.method == 'GET':
         recipe = Recipe.objects.all()
         all_recipe = DefaultRecipe.objects.all()
 
-        return render(request, 'list.html', {'recipes': all_recipe, 'new_recipe':recipe})
+        ten_min = DefaultRecipe.objects.filter(difficulty="10분 이내").values()
+        twenty_min = DefaultRecipe.objects.filter(difficulty="20분 이내").values()
+        thirty_min = DefaultRecipe.objects.filter(difficulty="30분 이내").values()
+        sixty_min = DefaultRecipe.objects.filter(difficulty="60분 이내").values()
+        difficult = DefaultRecipe.objects.filter(timecost="중급").values()
+        soso = DefaultRecipe.objects.filter(timecost="초급").values()
+        easy = DefaultRecipe.objects.filter(timecost="아무나").values()
+
+        doc={
+            'recipes': all_recipe,
+            'new_recipe': recipe,
+            'ten_min': ten_min,
+            'twenty_min': twenty_min,
+            'thirty_min': thirty_min,
+            'sixty_min': sixty_min,
+            'difficult': difficult,
+            'soso': soso,
+            'easy': easy,
+             }
+
+        return render(request, 'list.html', doc)
     elif request.method == 'POST':
         searched = request.POST.get('searched', '')
         print(searched)
         all_recipe = DefaultRecipe.objects.all()
         print(all_recipe)
-
         search_list=[]
-        for i in range(DefaultRecipe.objects.count()):
+        for i in range(total_recipe):
             title = DefaultRecipe.objects.all().values()[i]['title'] #제목 꺼내오기
             if searched in title:#타이틀에 내가 원하는 이름이 있다면
                 search_list.append(DefaultRecipe.objects.all().values()[i])#내가 원하는 데이터 만으로 쿼리셋으로 만든다
