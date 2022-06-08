46584b1a9d16b951f1f170539e825877a0a4a5f0
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-08 19:04:54 +0900

    커밋

--- detail/models.py
+++ detail/models.py
@@ -2,11 +2,24 @@ from django.db import models
 from user.models import UserModel
 from webScrapping.models import DefaultRecipe
 
+
 # Create your models here.
 class LikeModel(models.Model):
     class Meta:
-        db_table='like'
+        db_table = 'like'
+
     like_me = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     like_recipe = models.ForeignKey(DefaultRecipe, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CommentModel(models.Model):
+    class Meta:
+        db_table = 'comment'
+
+    comment_me = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    comment_recipe = models.ForeignKey(DefaultRecipe, on_delete=models.CASCADE)
+    comment_content = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
