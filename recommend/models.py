87003f579901943b8a094d93cac9701e075ac967
migdracios <migdracios@gmail.com>
2022-06-10 09:16:58 +0900

    Add - 기존 크롤링 데이터를 활용한 컨텐츠 기반 필터링 데이터를 DB삽입

--- recommend/models.py
+++ recommend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class RecommendModel(models.Model):
+    class Meta:
+        db_table = 'default_reco'
+    reco1 = models.CharField(max_length=100, blank=True)
+    reco2 = models.CharField(max_length=100, blank=True)
+    reco3 = models.CharField(max_length=100, blank=True)
+    reco4 = models.CharField(max_length=100, blank=True)
+    reco5 = models.CharField(max_length=100, blank=True)
